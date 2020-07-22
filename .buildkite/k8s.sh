6dc19369b0da70e89da0115372054280e9cfc3f0
Gum Baba <gumshahid.baba@gmail.com>
2020-07-22 23:58:52 +1000

    Correcting the typo

--- .buildkite/k8s.sh
+++ .buildkite/k8s.sh
@@ -6,7 +6,7 @@ aws eks --region us-east-2 update-kubeconfig --name $CLUSTER_NAME
 
 pushd .k8s
 kubectl apply -f ns.yaml
-sed -i "s/appversion/$BUILDKITE_BUILD_NUMBER/g" $ENV/my-app.yaml
+sed -i "s/appversion/$BUILDKITE_BUILD_NUMBER/g" $ENV/deployment.yaml
 
 for schema in service deployment; do
   kubectl apply -f $ENV/${schema}.yaml
